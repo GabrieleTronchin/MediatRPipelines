32b61bc9fbf9cc9293b9e068ebd91046e3d4b948
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-04-30 15:41:21 +0200

    Add sample pipelines

---
+++ Sample.MediatRPipelines.Domain/Sample.MediatRPipelines.Domain/PipelineBehavior/CommandAuthorizationBehavior.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Sample.MediatRPipelines.Domain.Primitives;
+
+namespace Sample.MediatRPipelines.Domain.PipelineBehavior
+{
+    public class CommandAuthorizationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+
+        public CommandAuthorizationBehavior()
+        {
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
